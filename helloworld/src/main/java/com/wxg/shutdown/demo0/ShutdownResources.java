754495cab09ff42736d8a026323dd6d4f9d803e8
wxg <chixin0769@163.com>
2019-03-27 19:45:44 +0800

    添加了一段注释，应该是正确的！

--- helloworld/src/main/java/com/wxg/shutdown/demo0/ShutdownResources.java
+++ helloworld/src/main/java/com/wxg/shutdown/demo0/ShutdownResources.java
@@ -19,6 +19,12 @@ public class ShutdownResources {
 
     /**
      * 调用了 {@link EventLoopGroup#shutdownGracefully()} 之后，还需要将 {@link #eventlgs} 清空。
+     * <ul>
+     * <li>2019年03月27日19:40:57</li>
+     * <li>{@link io.netty.bootstrap.ServerBootstrap} 和 {@link io.netty.bootstrap.Bootstrap} 不在同一个 JVM 中，所以，想简单的维护服务端和客户端的 {@link EventLoopGroup} 是不现实的。</li>
+     * <li>将它们维护在 Reids 中，或者 消息队列 中，才是正确的！</li>
+     * <li>另外，只有多个 Server 是需要维护的，client 交给用户自己来维护</li>
+     * </ul>
      */
     public void destroy() {
         System.out.println("eventlgs size : " + eventlgs.size());
