134c18921995669edb32d0745e34732666462509
wxg <chixin0769@163.com>
2019-03-27 17:35:53 +0800

    是不是没有添加 编解码器，所以没有输出？

---
+++ helloworld/src/main/java/com/wxg/shutdown/demo0/SimpleShutDownServer0.java
@@ -0,0 +1,59 @@
+package com.wxg.shutdown.demo0;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.time.LocalDateTime;
+
+/**
+ * 2019年03月27日17:10:14
+ */
+public class SimpleShutDownServer0 {
+    private static EventLoopGroup bossGroup;
+    private static EventLoopGroup workGroup;
+
+    public static void main(String[] args) throws InterruptedException {
+        bossGroup = new NioEventLoopGroup();
+        workGroup = new NioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+            serverBootstrap.group(bossGroup, workGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.WARN))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new SimpleStringMsgHandler());
+                        }
+                    });
+
+            ChannelFuture channelFuture = serverBootstrap.bind(8899).sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workGroup.shutdownGracefully();
+        }
+    }
+
+    private static class SimpleStringMsgHandler extends SimpleChannelInboundHandler<String> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+            System.out.println(ctx.channel().remoteAddress() + ", " + msg);
+            ctx.writeAndFlush("received msg at " + LocalDateTime.now());
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+}
---
+++ helloworld/src/main/java/com/wxg/shutdown/demo0/SimpleShutdownClient0.java
@@ -0,0 +1,55 @@
+package com.wxg.shutdown.demo0;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * 2019年03月27日17:22:45
+ */
+public class SimpleShutdownClient0 {
+    private static EventLoopGroup group;
+
+    public static void main(String[] args) throws InterruptedException {
+        group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+
+                            pipeline.addLast(new SimpleStringMsgHandler());
+                        }
+                    });
+
+            ChannelFuture future = bootstrap.connect("localhost", 8899).sync();
+            future.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    private static class SimpleStringMsgHandler extends SimpleChannelInboundHandler<String> {
+        @Override
+        protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
+            System.out.println(ctx.channel().remoteAddress() + ", " + msg);
+        }
+
+        @Override
+        public void channelActive(ChannelHandlerContext ctx) throws Exception {
+            ctx.writeAndFlush("hello,world!");
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+}
