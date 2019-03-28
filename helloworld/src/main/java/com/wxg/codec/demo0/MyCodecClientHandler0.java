cbd09377172bfc874f5729231f1902dccd64dd90
wxg <chixin0769@163.com>
2019-03-28 15:21:46 +0800

    写的了提交注释，又感觉方法上应该添加同样的注释，遂修改代码添加方法注释

--- helloworld/src/main/java/com/wxg/codec/demo0/MyCodecClientHandler0.java
+++ helloworld/src/main/java/com/wxg/codec/demo0/MyCodecClientHandler0.java
@@ -1,9 +1,11 @@
 package com.wxg.codec.demo0;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class MyCodecClientHandler0 extends SimpleChannelInboundHandler<Long> {
     @Override
@@ -13,6 +15,11 @@ public class MyCodecClientHandler0 extends SimpleChannelInboundHandler<Long> {
 //        ctx.writeAndFlush("From client : " + LocalDateTime.now());
     }
 
+    /**
+     * 这里输出 5 个 long 型的变量；可以在 {@link MyByteToLongDecoder#decode(ChannelHandlerContext, ByteBuf, List)} 的 `System.out.println(in.readableBytes());` 的输出看到变化由 40, 32, 24, 16, 8 逐次递减。
+     * @param ctx
+     * @throws Exception
+     */
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
         ctx.writeAndFlush(123456L);
