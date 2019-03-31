fdff50fb241e234af7a7b8279a3efae581a63d0d
wxg <chixin0769@163.com>
2019-03-31 16:50:43 +0800

    添加注释

--- NettyInAction/src/main/java/ch09/AbsIntegerEncoderTest.java
+++ NettyInAction/src/main/java/ch09/AbsIntegerEncoderTest.java
@@ -2,14 +2,21 @@ package ch09;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.util.concurrent.EventExecutorGroup;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * copy at 2019年03月31日16:37:35
+ * copy at 2019年03月31日16:37:35 ,
+ * <p>{@link EmbeddedChannel#EmbeddedChannel(ChannelHandler...)} 构造方法值得研究，</p>
+ * <p>其中会调用到 {@link EmbeddedChannel#setup(boolean, ChannelHandler...)} 方法，</p>
+ * <p>这个方法的内部细节很能说明问题，在这里 pipeline 被构建，handlers 被添加进 pipeline 中，</p>
+ * <p>ctx就是在handler添加进 pipeline 时被构建的 -> {@link io.netty.channel.DefaultChannelPipeline#addLast(EventExecutorGroup, String, ChannelHandler)}</p>
+ *
  */
 public class AbsIntegerEncoderTest {
 
